0ba85af5f06ba5ce7dc85777b1c212e549911b22
smdurjoy <smdurjoy963@gmail.com>
2022-07-20 21:37:42 +0600

    passengers list flight wise

--- partials/sidebar.php
+++ partials/sidebar.php
@@ -8,23 +8,34 @@
         </div>
         <ul class="sidebar-menu">
             <?php $activePage = basename($_SERVER['PHP_SELF'], ".php"); ?>
-            <li class="dropdown <?= ($activePage == 'index') ? 'active':''; ?>">
+            <li class="dropdown <?= ($activePage == 'index') ? 'active' : ''; ?>">
                 <a href="index.php" class="nav-link"><i data-feather="monitor"></i><span>Dashboard</span></a>
             </li>
-            <li class="dropdown <?= ($activePage == 'aircraft') ? 'active':''; ?>">
-<!--                1-->
+            <li class="dropdown <?= ($activePage == 'aircraft') ? 'active' : ''; ?>">
                 <a href="aircraft.php" class="nav-link"><i data-feather="monitor"></i><span>Aircraft</span></a>
             </li>
-            <li class="dropdown <?= ($activePage == 'flightList') ? 'active':''; ?>">
+            <?php
+            $activeFlightDropdown = false;
+
+            if ($activePage == 'flightList' || $activePage == 'flightPassengers' || $activePage = 'totalFare') {
+                $activeFlightDropdown = true;
+            }
+            ?>
+            <li class="dropdown <?= $activeFlightDropdown ? 'active' : ''; ?>">
                 <a href="#" class="menu-toggle nav-link has-dropdown"><i data-feather="copy"></i>
                     <span>Flight</span></a>
                 <ul class="dropdown-menu">
-<!--                    2-->
-                    <li class="<?= ($activePage == 'flightList') ? 'active':''; ?>"><a class="nav-link" href="flightList.php">Flight List Route Wise</a></li>
-<!--                    3-->
-                    <li><a class="nav-link" href="flightPassengers.html">Flight Passengers</a></li>
-<!--                    5-->
-                    <li><a class="nav-link" href="totalFare.html">Total Fare</a></li>
+                    <li class="<?= ($activePage == 'flightList') ? 'active' : ''; ?>">
+                        <a class="nav-link" href="flightList.php">Flight
+                            List Route Wise
+                        </a>
+                    </li>
+                    <li class="<?= ($activePage == 'flightPassengers') ? 'active' : ''; ?>">
+                        <a class="nav-link" href="flightPassengers.php">
+                            Flight Passengers
+                        </a>
+                    </li>
+                    <li><a class="nav-link" href="totalFare.php">Total Fare</a></li>
                 </ul>
             </li>
             <li class="dropdown">
@@ -32,9 +43,9 @@
                     <span>Tickets</span></a>
                 <ul class="dropdown-menu">
                     <li><a class="nav-link" href="ticketsInfo.html">Tickets Info</a></li>
-<!--                    6-->
+                    <!--                    6-->
                     <li><a class="nav-link" href="badge.html">Date Wise Tickets</a></li>
-<!--                    10-->
+                    <!--                    10-->
                     <li><a class="nav-link" href="badge.html">State Wise Tickets</a></li>
                 </ul>
             </li>
@@ -42,11 +53,11 @@
                 <a href="#" class="menu-toggle nav-link has-dropdown"><i data-feather="copy"></i>
                     <span>Travelling</span></a>
                 <ul class="dropdown-menu">
-<!--                    7-->
+                    <!--                    7-->
                     <li><a class="nav-link" href="ticketsInfo.html">Airport Wise</a></li>
-<!--                    8-->
+                    <!--                    8-->
                     <li><a class="nav-link" href="badge.html">Reached Wise</a></li>
-<!--                    9-->
+                    <!--                    9-->
                     <li><a class="nav-link" href="badge.html">States Wise</a></li>
                 </ul>
             </li>
